eee8e2c9ef8f2e6f25a5c8c67bb7cd20bb3eaacb
Dmitry Zotov <Dmitriyzotov2009@yandex.ru>
2017-04-20 03:07:25 +0300

    F

--- src/Mclass.java
+++ src/Mclass.java
@@ -1,19 +1,35 @@
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class Mclass {
-    public static void main(String[] args)  {
-        System.out.println(dayIncrement("Текущая дата 01.04.2015",4));
+
+    public static void main(String[] args) throws ParseException {
+        String inputDate = "Текущая дата 01.04.2015";
+        System.out.println(dayIncrement(inputDate,33));
     }
-    private static String dayIncrement (String in, int d){
-        //вытаскиваем месяц, число, год из входной строки in
-        String inDay    = in.substring((in.length()-10), (in.length()-8));
-        String inMonth  = in.substring((in.length()-8), (in.length()-5));
-        String inYear   = in.substring(in.length()-5);
-        //к входной дате прибавляем необходимое количесвто дней
-        int days = Integer.parseInt(inDay) + d;
-        String day = Integer.toString(days);
-        //добавляем "0" если необходимо
-        if (days < 10){
-            day = "0"+day;
-        }
-        return "Дата покупки " + day + inMonth + inYear;
+
+    private static String dayIncrement(String in, int d) throws ParseException {
+        String inputDate = in.replaceAll("[a-zA-Zа-яА-Я]+", "");
+        //достали дату из входящей строки
+        String outputDate;
+        SimpleDateFormat sdf = new SimpleDateFormat("dd.MM.yyyy");
+        //создали формат даты
+        Date date = sdf.parse(inputDate);
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        //присвоили календарю входящую дату
+        int year  = cal.get(Calendar.YEAR);
+        int month = (cal.get(Calendar.MONTH)+1);
+        //месяцы от 0 идут
+        int day   = cal.get(Calendar.DAY_OF_MONTH);
+        outputDate = ((day+d)+"."+month+"."+year);
+        //увеличили дату на необходимое количество дней
+        date = sdf.parse(outputDate);
+        //присвоили новую дату
+        outputDate = sdf.format(date);
+        //отформатировали новую дату
+        return "Дата покупки " +outputDate;
     }
 }
